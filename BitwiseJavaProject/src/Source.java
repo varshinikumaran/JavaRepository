638e70849b686c12560e8d1b28e530823dc65bc9
SVARSHIN <SVARSHIN@INLEN8520069541.corp.capgemini.com>
2023-01-07 13:12:16 +0530

    Bitwise

---
+++ BitwiseJavaProject/src/Source.java
@@ -0,0 +1,14 @@
+
+public class Source {
+
+	public static void main(String[] args) {
+				int num1=10 ,num2=20;
+				System.out.println("num1"+num1);
+				
+				System.out.println("num2"+num2);
+				System.out.println(num1 | num2);
+		}
+}
+
+
+
